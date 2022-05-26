5051c8250e78535ed239515f2e54207930a8d67a
lxw <18435135529@163.com>
2022-05-26 10:59:31 +0800

    <daily>: add 2022-05-25 daily`

--- debug/debug.cc
+++ debug/debug.cc
@@ -1,28 +1,36 @@
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
-bool findNumberIn2DArray(vector<vector<int>>& matrix, int target) {
-    if (matrix.size() == 0) {
-        return false;
-    }
-    int row = matrix.size() - 1;
-    int column = 0;
-    while (row>=0 && column<matrix[0].size()) {
-        if (target == matrix[row][column]) {
-            return true;
-        } else if (target < matrix[row][column]) {
-            row--;
-            continue;
-        } else if (target > matrix[row][column]) {
-            column++;
-            continue;
+    string replaceSpace(string s) {
+        if (s.length()==0) {
+            return s;
         }
+        int nums = 0;
+        for (int i=0;i<s.length();i++) {
+            if (s[i]==' ') {
+                nums++;
+            }
+        }
+        int n = s.length()+ nums*2;
+        string str_bak(n, 'a');
+        for (int i=0, j=0;i<s.length();i++) {
+            if (s[i]!=' ') {
+                str_bak[j] = s[i];
+                j++;
+            } else {
+                str_bak[j] = '%';
+                str_bak[++j] = '2';
+                str_bak[++j] = '0';
+                j++;
+            }
+        }
+        return str_bak;
     }
-    return false;
-}
 
 int main() {
-    vector<vector<int>> matrix = vector<vector<int>>(5, vector<int>(5)) = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
-    int target = 20;
-    findNumberIn2DArray(matrix, target);
+    string s = "we are good";
+    string out = replaceSpace(s);
+    cout<<out;
+
 }
